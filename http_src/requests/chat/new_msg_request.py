8735617ad01b1c675cf78d6cf70dc583cc9cdc1f
DevAlexandreCR <devalexandrecr@gmail.com>
2025-09-09 03:10:21 +0000

    Add example to content field in NewMessageRequest model

--- http_src/requests/chat/new_msg_request.py
+++ http_src/requests/chat/new_msg_request.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, Field
 from constants.session_status import SessionStatus
 
 class NewMessageRequest(BaseModel):
-    content: str = Field(..., min_length=1, max_length=1000)
+    content: str = Field(..., min_length=1, max_length=1000, example="Hola, necesito un servicio.")
     session_status: SessionStatus = Field(..., description="Current status of the user session")
